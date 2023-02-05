115613b59cb60c57dc6781f26d8c24a177b184d8
Jonathan Lai <78984450+jonathan-c-lai@users.noreply.github.com>
2023-02-04 23:55:33 -0800

    Lab 4 started and task 1-3
    task 4 underway, working on #2, classify idle vs non-idle

---
+++ Lab4/mqtt_sub.py
@@ -0,0 +1,44 @@
+import paho.mqtt.client as mqtt
+
+# used code that the professor/TA posted, modified to receive message
+# and then send out a message and eventually with all 3 computers
+# we were able to ping pong 3-way and increment a counter
+
+# callback definitions
+def on_connect(client, userdata, flags, rc):
+    print("Connection returned result: " + str(rc))
+    client.subscribe("ece180jonimu/test", qos=1)
+
+def on_disconnect(client, userdata, rc):
+    if rc != 0:
+        print('Unexpected Disconnect')
+    else:
+        print('Expected Disconnect')
+# The default message callback.
+def on_message(client, userdata, message):
+    print('Received message: "' + str(message.payload) + '" on topic "' +
+        message.topic + '" with QoS ' + str(message.qos))
+    
+# 1. create a client instance.
+client = mqtt.Client()
+# add additional client options (security, certifications, etc.)
+# add callbacks to client.
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+client.on_message = on_message
+# 2. connect to a broker using one of the connect*() functions.
+# client.connect_async("test.mosquitto.org")
+client.connect_async('mqtt.eclipseprojects.io')
+# client.connect("test.mosquitto.org", 1883, 60)
+# client.connect("mqtt.eclipse.org")
+# 3. call one of the loop*() functions to maintain network traffic flow with the broker.
+client.loop_start()
+# client.loop_forever()
+
+while True: # perhaps add a stopping condition using some break or something
+    pass # do your non-blocked other stuff here, like receive IMU data or something.
+# use subscribe() to subscribe to a topic and receive messages.
+# use publish() to publish messages to the broker.
+# use disconnect() to disconnect from the broker.
+client.loop_stop()
+client.disconnect()
